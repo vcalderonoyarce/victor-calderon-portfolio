28e12e91ae43aa77171b2afd23005bb58fc9cf9b
Víctor Manuel Calderón Oyarce <vcalderonoyarce@gmail.com>
2024-01-04 00:41:58 -0300

    Added some info about my stack

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,21 +1,25 @@
 'use client'
 import React, { Component } from "react";
 import Image from 'next/image';
+import styles from './header.module.css'
 
 export default class Header extends Component {
     render() {
         return (
-            <header className="sticky top-0 left-0 right-0">
-                <nav id="site-header" className="text-white backdrop-blur px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <header className="sticky top-0 left-0 right-0 z-50">
+                <nav id={styles.site_header} className="text-white backdrop-blur px-4 lg:px-6 py-5">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <a href="/" className="flex items-center">
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">VC<span className="accent-color">.</span></span>
+                            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">
+                                V
+                                <span className="font-normal">C</span>
+                            <span className="text-[var(--accent-color)]">.</span></span>
                         </a>
                         <div className="flex items-center lg:order-2">
-                            <a href="https://www.linkedin.com/in/vcalderonoyarce/" target="_blank" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 lg:px-4 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+                            <a href="https://www.linkedin.com/in/vcalderonoyarce/" target="_blank" className="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 lg:px-4 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
                                 <Image src="/img/linkedin.svg" alt="LinkedIn" width="20" height="20"></Image>
                             </a>
-                            <a href="https://www.linkedin.com/in/vcalderonoyarce/" target="_blank" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 lg:px-4 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+                            <a href="https://www.linkedin.com/in/vcalderonoyarce/" target="_blank" className="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 lg:px-4 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
                                 <Image src="/img/threads_logo.svg" alt="LinkedIn" width="20" height="20"></Image>
                             </a>
 
@@ -24,16 +28,16 @@ export default class Header extends Component {
                         <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                                 <li>
-                                    <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                                    <a href="/" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Inicio</a>
                                 </li>
                                 <li>
-                                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                    <a href="/about" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Acerca de</a>
                                 </li>
                                 <li>
-                                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">My work</a>
+                                    <a href="/blog" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Blog</a>
                                 </li>
                                 <li>
-                                    <a href="/contact" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                    <a href="/contact" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Contacto</a>
                                 </li>
                             </ul>
                         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,65 @@
 import Image from 'next/image'
+import { Fira_Code } from 'next/font/google'
+
+const fira_code = Fira_Code({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['300', '400', '500', '600', '700']
+});
+
 
 export default function Home() {
   return (
     <main>
 
-      <section className="pb-12 bg-gradient-to-b from-[#1E2128] via-[#1E2128] to-[#1E2128]">
+      <section className="pb-12 bg-gradient-to-b from-[#1E2128] to-[#1E2128]">
         <div className="items-center pt-12 px-8 mx-auto max-w-7xl lg:px-16 md:px-12">
           <div className="justify-center w-full text-center lg:p-10 max-auto">
-            <div className="justify-center w-full mx-auto">
+            <div className="justify-center w-full mx-auto relative">
 
               <div className="flex flex-col items-center justify-center max-w-xl gap-3 mx-auto lg:flex-row">
                 <Image alt="Víctor Calderón Oyarce" width="160" height="160" className="w-32 h-32 rounded-full border border-[#E8E3F4]" src="/img/vcalderonoyarce.jpg"></Image>
               </div>
-
 
               <p className="text-[#FFF] mt-4 sm:px-32 sm:text-xl text-smtracking-tighter">
                 &lt;@<span className="text-[var(--accent-color)] accent-color font-semibold">vcalderonoyarce</span> / &gt;
               </p>
 
               <p className="sm:mt-8 mt-3 sm:px-44 text-[#FFF] text-4xl sm:text-6xl font-semibold tracking-tighter">
-                <div className="inline relative">
+                <span className="inline relative">
                   <span className="text-[var(--accent-color)] relative">D</span>
-                  esarrollador web <span className="underline leading-8 underline-offset-8	decoration-8 decoration-[var(--accent-color)]">Fullstack</span><span className="accent-color">.</span>
-                </div>
+                  esarrollador web <span className="underline leading-8 underline-offset-8	decoration-8 decoration-[var(--accent-color)]">Fullstack</span><span className="text-[var(--accent-color)]">.</span>
+                </span>
               </p>
 
-              <p className="sm:mt-8 mt-2.5 text-[#FFF] sm:px-72  sm:leading-loose text-lg font-normal tracking-tighter">
-                Soy un <span className="font-semibold text-[var(--accent-color)] ">desarrollador web fullstack</span>, trabajador <span className="font-semibold">100% remoto</span>, y antiguo <span className="font-semibold">redactor</span> de blog. Adoro construir <span className="font-semibold">web apps</span> que solucionen problemas del mundo real.
+              <p className={fira_code.className + " sm:mt-8 mt-2.5 text-[#FFF] sm:px-72  sm:leading-loose text-lg font-normal tracking-tighter"}>
+                Soy un <span className="font-semibold text-[var(--accent-color)] ">{"{"}desarrollador web fullstack{"}"}</span>, <span className="font-semibold">100% remoto</span>, y antiguo <span className="font-semibold">redactor</span> de blog. Adoro construir <span className="font-semibold">web apps</span> que solucionen problemas del mundo real.
               </p>
+
+              <div className='text-center space-x-3 mt-6'>
+                <div className="rounded-full shadow-2xl bg-gradient-to-tr from-[#17191e] to-[#17191e] p-1 inline-block">
+                  <Image src="/img/sass.svg" alt="NodeJS" width="24" height="24"></Image>
+                </div>
+                <div className="rounded-full shadow-2xl bg-gradient-to-tr from-[#17191e] to-[#17191e] p-1 inline-block">
+                  <Image src="/img/google-cloud.svg" alt="NodeJS" width="24" height="24"></Image>
+                </div>
+
+                <div className="rounded-full shadow-2xl bg-gradient-to-tr from-[#17191e] to-[#17191e] p-1 inline-block">
+                  <Image src="/img/nodejs.svg" alt="NodeJS" width="24" height="24"></Image>
+                </div>
+
+                <div className="rounded-full shadow-2xl bg-gradient-to-tr from-[#17191e] to-[#17191e] p-1 inline-block">
+                  <Image src="/img/reactjs.svg" alt="NodeJS" width="24" height="24"></Image>
+                </div>
+              </div>
+
             </div>
           </div>
 
         </div>
 
-        <div className="text-center space-x-4 mt-4">
+        <div className="text-center mt-4">
           <button className="bg-[var(--accent-color)] translate-y-1 text-[#fff] sm:text-lg text-xs py-2.5 px-6 rounded-full inline-flex items-center">
             Saber más
           </button>
